42c476272c645a797fcf81f4b17c2290466ea6f1
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-05-03 22:12:41 -0700

    Add program that uses std::async to launch a worker thread.

---
+++ Async.cpp
@@ -0,0 +1,26 @@
+#include <thread>
+#include <future>
+#include <iostream>
+#include <string>
+
+std::string fun(bool flag) {
+  if (flag) {
+    std::cout << "Worker returning.\n";
+    return "Message from worker.";
+  } else {
+    std::cout << "Worker throwing.\n";
+    throw std::runtime_error("Exception from thread.");
+  }
+}
+
+int main() {
+  std::future<std::string> fut = std::async(fun, false);
+  std::cout << "Main receiving.\n";
+  try {
+    std::string s = fut.get();
+    std::cout << s << std::endl;
+  } catch (std::exception& e) {
+    std::cout << "Caught: " << e.what() << std::endl;
+  }
+  return 0;
+}
